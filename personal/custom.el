86332bf379a0d32571c4b06d46cf091242398146
Matthew Kennedy <burnsidemk@gmail.com>
2018-02-07 11:44:18 -0600

    Clojure repl command

--- personal/custom.el
+++ personal/custom.el
@@ -4,6 +4,27 @@
   (setq mac-command-modifier 'meta
         mac-option-modifier 'super))
 
+(setq cider-repl-display-help-banner nil
+      cider-cljs-lein-repl
+      "(cond
+  (resolve 'user/run) ;; Chestnut projects
+  (eval '(do (user/run)
+             (user/browser-repl)))
+  (try
+    (require 'figwheel-sidecar.repl-api)
+    (resolve 'figwheel-sidecar.repl-api/start-figwheel!)
+    (catch Throwable _))
+  (eval '(do (figwheel-sidecar.repl-api/start-figwheel!)
+             (figwheel-sidecar.repl-api/cljs-repl)))
+  (try
+    (require 'cemerick.piggieback)
+    (resolve 'cemerick.piggieback/cljs-repl)
+    (catch Throwable _))
+  (eval '(cemerick.piggieback/cljs-repl (cljs.repl.rhino/repl-env)))
+  :else
+  (throw (ex-info \"Failed to initialize CLJS repl. Add com.cemerick/piggieback and optionally figwheel-sidecar to your project.\" {})))")
+
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -11,7 +32,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (yaml-mode web-mode geiser cargo flycheck-rust racer rust-mode json-mode js2-mode rainbow-mode elisp-slime-nav slime cider clojure-mode rainbow-delimiters company counsel swiper ivy vkill exec-path-from-shell zop-to-char zenburn-theme which-key volatile-highlights undo-tree smartrep smartparens smart-mode-line operate-on-number move-text magit projectile ov imenu-anywhere guru-mode grizzl god-mode gitignore-mode gitconfig-mode git-timemachine gist flycheck expand-region epl editorconfig easy-kill diminish diff-hl discover-my-major dash crux browse-kill-ring beacon anzu ace-window))))
+    (markdown-mode yaml-mode web-mode geiser cargo flycheck-rust racer rust-mode json-mode js2-mode rainbow-mode elisp-slime-nav slime cider clojure-mode rainbow-delimiters company counsel swiper ivy vkill exec-path-from-shell zop-to-char zenburn-theme which-key volatile-highlights undo-tree smartrep smartparens smart-mode-line operate-on-number move-text magit projectile ov imenu-anywhere guru-mode grizzl god-mode gitignore-mode gitconfig-mode git-timemachine gist flycheck expand-region epl editorconfig easy-kill diminish diff-hl discover-my-major dash crux browse-kill-ring beacon anzu ace-window))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
