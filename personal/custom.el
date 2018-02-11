d61c036d547f20c3e8a6493228f80d8e2b3061f2
Matthew Kennedy <burnsidemk@gmail.com>
2018-02-11 14:29:30 -0600

    Stuff

--- personal/custom.el
+++ personal/custom.el
@@ -24,18 +24,26 @@
   :else
   (throw (ex-info \"Failed to initialize CLJS repl. Add com.cemerick/piggieback and optionally figwheel-sidecar to your project.\" {})))")
 
+(setq default-directory "~/")
+
+(when (member window-system '(ns w32))
+  (menu-bar-mode 1))
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(blink-cursor-mode nil)
+ '(column-number-mode t)
  '(package-selected-packages
    (quote
-    (markdown-mode yaml-mode web-mode geiser cargo flycheck-rust racer rust-mode json-mode js2-mode rainbow-mode elisp-slime-nav slime cider clojure-mode rainbow-delimiters company counsel swiper ivy vkill exec-path-from-shell zop-to-char zenburn-theme which-key volatile-highlights undo-tree smartrep smartparens smart-mode-line operate-on-number move-text magit projectile ov imenu-anywhere guru-mode grizzl god-mode gitignore-mode gitconfig-mode git-timemachine gist flycheck expand-region epl editorconfig easy-kill diminish diff-hl discover-my-major dash crux browse-kill-ring beacon anzu ace-window))))
+    (markdown-mode yaml-mode web-mode geiser cargo flycheck-rust racer rust-mode json-mode js2-mode rainbow-mode elisp-slime-nav slime cider clojure-mode rainbow-delimiters company counsel swiper ivy vkill exec-path-from-shell zop-to-char zenburn-theme which-key volatile-highlights undo-tree smartrep smartparens smart-mode-line operate-on-number move-text magit projectile ov imenu-anywhere guru-mode grizzl god-mode gitignore-mode gitconfig-mode git-timemachine gist flycheck expand-region epl editorconfig easy-kill diminish diff-hl discover-my-major dash crux browse-kill-ring beacon anzu ace-window)))
+ '(size-indication-mode t)
+ '(tool-bar-mode nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(default ((t (:family "Fixedsys" :foundry "raster" :slant normal :weight normal :height 113 :width normal)))))
